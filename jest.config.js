485b43747b1e57a764a810ae9f1ccf758714b93c
Osmar Joseph Wilsinski <osmarjoseph@MacBook-Pro-de-Osmar.local>
2021-04-04 22:13:16 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
