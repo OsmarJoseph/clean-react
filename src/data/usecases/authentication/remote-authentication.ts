2d62a3f0d83f05266a408bc801541559b3e6dac0
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-04-08 19:55:16 -0300

    feat: ensure RemoteAuthentication throw InvalidCredentialsError if HttpPostClient returns unauthorized

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { Authentication } from '@/domain/usecases'
-import { HttpPostClient } from '@/data/protocols'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols'
 import { AuthenticationHttpPostClient } from '@/data/usecases'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 export class RemoteAuthentication implements Authentication {
   constructor (
@@ -10,7 +11,12 @@ export class RemoteAuthentication implements Authentication {
 
   async auth ({ email, password }: Authentication.Params): Authentication.Result {
     const { httpPostClient, url } = this
-    await httpPostClient.post({ url, body: { email, password } })
+    const httpPesponse = await httpPostClient.post({ url, body: { email, password } })
+
+    switch (httpPesponse.statusCode) {
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+    }
+
     return null
   }
 }
---
+++ src/domain/errors/invalid-credentials-error.ts
@@ -0,0 +1,7 @@
+export class InvalidCredentialsError extends Error {
+  static message = 'You are using invalid credentials'
+  constructor () {
+    super(InvalidCredentialsError.message)
+    this.name = 'InvalidCredentialsError'
+  }
+}
