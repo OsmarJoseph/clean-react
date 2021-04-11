0dbe2ca0af2da46e049b521845179f3fc5f85ec0
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-04-11 00:35:50 -0300

    feat: make AxiosHttpClient return the correct statusCode and body

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -4,7 +4,11 @@ import axios from 'axios'
 
 export class AxiosHttpClient<Constructor extends HttpPostClient.Constructor> implements HttpPostClient<Constructor> {
   async post ({ url, body }: HttpPostClient.Params<Constructor['request']>): Promise<HttpResponse<Constructor['response']>> {
-    await axios.post(url, body)
-    return null
+    const httpResponse = await axios.post(url, body)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
