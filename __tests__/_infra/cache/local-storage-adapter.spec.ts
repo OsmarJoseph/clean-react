24fd088371a6685a9d2186c7b56349cfc0d6c654
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-05-28 22:12:49 -0300

    feat: add local storage adapter

---
+++ __tests__/_infra/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,29 @@
+import { LocalStorageAdapter } from '@/infra/cache'
+
+import 'jest-localstorage-mock'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: LocalStorageAdapter
+}
+const makeSut = (): SutTypes => {
+  const sut = new LocalStorageAdapter()
+  return {
+    sut,
+  }
+}
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+  test('should call localStorage with correct values', () => {
+    const { sut } = makeSut()
+    const key = faker.random.word()
+    const value = faker.datatype.uuid()
+
+    sut.set(key, value)
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+  })
+})
---
+++ src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,7 @@
+import { SetStorage } from '@/data/protocols'
+
+export class LocalStorageAdapter implements SetStorage {
+  set(key: string, value: any): void {
+    localStorage.setItem(key, value)
+  }
+}
