63547c6698fef50decaf1c49953ee9ae73e3e284
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-06-16 13:17:18 -0300

    test: add login page integration tests

--- __tests__/cypress/integration/login-page.spec.ts
+++ __tests__/cypress/integration/login-page.spec.ts
@@ -1,3 +1,7 @@
+import faker from 'faker'
+
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('LoginPage', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -11,6 +15,69 @@ describe('LoginPage', () => {
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'O campo email é invalido')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'O campo password é invalido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+  it('should present valid state if form is valid', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢')
+
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+  it('should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'You are using invalid credentials.')
+
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+  it('should present save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').type('mango@gmail.com')
+    cy.getByTestId('password').type('12345')
+
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+
+    cy.url().should('eq', `${baseUrl}/`)
+
+    cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 })
