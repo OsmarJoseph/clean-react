70bdf478954ac084d7b82ded31e05293b3ed9c2c
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-06-16 17:32:40 -0300

    test: add login page aditional tests

--- __tests__/cypress/integration/login-page.spec.ts
+++ __tests__/cypress/integration/login-page.spec.ts
@@ -44,40 +44,91 @@ describe('LoginPage', () => {
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
-  it('should present error if invalid credentials are provided', () => {
+  it('should present InvalidCredentialsError if invalid credentials are provided', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 401,
+      response: { error: faker.random.words() },
+    })
     cy.getByTestId('email').type(faker.internet.email())
     cy.getByTestId('password').type(faker.random.alphaNumeric(5))
 
     cy.getByTestId('submit').click()
 
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner')
-      .should('exist')
-      .getByTestId('main-error')
-      .should('not.exist')
-      .getByTestId('spinner')
+    cy.getByTestId('spinner')
       .should('not.exist')
       .getByTestId('main-error')
       .should('contain.text', 'You are using invalid credentials.')
 
     cy.url().should('eq', `${baseUrl}/login`)
   })
-  it('should present save accessToken if valid credentials are provided', () => {
-    cy.getByTestId('email').type('mango@gmail.com')
-    cy.getByTestId('password').type('12345')
+
+  it('should present UnexpectedError on 400', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 400,
+      response: { error: faker.random.words() },
+    })
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
 
     cy.getByTestId('submit').click()
 
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner')
-      .should('exist')
+    cy.getByTestId('spinner')
+      .should('not.exist')
       .getByTestId('main-error')
+      .should('contain.text', 'Something wrong happened, try again.')
+
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+  it('should present UnexpectedError if invalid data is returned', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 400,
+      body: { invalidProperty: faker.datatype.uuid() },
+    })
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5)).type('{enter}')
+
+    cy.getByTestId('spinner')
       .should('not.exist')
-      .getByTestId('spinner')
-      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Something wrong happened, try again.')
+
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+  it('should not call submit if form is invalid', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 200,
+      body: { accessToken: faker.datatype.uuid() },
+    }).as('request')
+    cy.getByTestId('email').type(faker.internet.email()).type('{enter}')
+
+    cy.get('@request.all').should('have.length', 0)
+  })
+  it('should present save accessToken if valid credentials are provided', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 200,
+      body: { accessToken: faker.datatype.uuid() },
+    })
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('main-error').should('not.exist').getByTestId('spinner').should('not.exist')
 
     cy.url().should('eq', `${baseUrl}/`)
 
     cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')))
+  })
+  it('should prevent multiple submits', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 200,
+      body: { accessToken: faker.datatype.uuid() },
+    }).as('request')
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('submit').dblclick()
+
+    cy.get('@request.all').should('have.length', 1)
   })
 })
